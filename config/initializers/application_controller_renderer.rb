97768fdaf942f0d3d5935caa5652bea82fae5ab7
深澤瑚大 <sango@fukazawanoMacBook-Air.local>
2021-07-08 22:27:29 +0900

    added fanctionality

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c2bbfd724ddac5a441d9dd135a8e2579a4acc54d831500b86a05f049cc82159127bb6336ff4b83df2ac26b371c1fc69925b9c721b9fafa18aebdb54f983637da'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_06_163406) do
+ActiveRecord::Schema.define(version: 2021_07_08_104607) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,6 +25,9 @@ ActiveRecord::Schema.define(version: 2021_07_06_163406) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
